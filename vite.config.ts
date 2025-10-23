357ada7609fa037295bf3ad0c66de676696abf23
Rom <chanphiromsok2109@gmail.com>
2025-10-23 20:39:58 +0700

    config env vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
+import { loadEnvFile } from 'process'
 
 export default defineConfig(({ mode }) => {
+  if (mode === 'development') {
+    loadEnvFile('.env')
+  } else if (mode === 'production') {
+    loadEnvFile('.env.production')
+  }
+
+  console.log("MODE:", mode);
   return {
     server: {
       port: 3000,
