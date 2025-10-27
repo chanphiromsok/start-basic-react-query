181000028e0db5d2f6755f724936246d6ab6d153
Rom <chanphiromsok2109@gmail.com>
2025-10-27 20:35:56 +0700

    feat: seem like we don't need setUpi18n in lingui-middleware.ts

--- src/locales/lingui-middleware.ts
+++ src/locales/lingui-middleware.ts
@@ -1,5 +1,4 @@
 import { createMiddleware } from "@tanstack/react-start"
-import { setupI18n } from "@lingui/core"
 import { getLocaleFromRequest } from "./i18nserver"
 import { dynamicActivate } from "./i18n"
 
@@ -7,10 +6,7 @@ import { dynamicActivate } from "./i18n"
 export const linguiMiddleware = createMiddleware({ type: "request" }).server(
   async ({ next }) => {
     const locale = getLocaleFromRequest()
-    const i18n = setupI18n({})
     await dynamicActivate(locale)
-    return next({
-      context: { i18n },
-    })
+    return next()
   }
 )
